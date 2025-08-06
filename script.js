d61af6fdbd79fb4813d9535ff8440388278ba7e2
ianmunga <ianmunga28@gmail.com>
2025-08-06 17:52:07 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const reveals = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('is-visible');
+    }
+  });
+}, { threshold: 0.1 });
+
+reveals.forEach(section => observer.observe(section));
